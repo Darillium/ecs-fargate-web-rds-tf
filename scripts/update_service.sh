14bceeac252755a893a8b49ead6050258d1b5665
Maxim Avezbakiev <maxim@snowleopardcloud.co.uk>
2018-03-22 00:39:35 +0000

    adding --region to aws cli in the script to get the value from TF_VAR_region env variable

--- scripts/update_service.sh
+++ scripts/update_service.sh
@@ -1,3 +1,3 @@
-echo "running command aws ecs update-service --cluster `terraform output cluster_name` --service `terraform output service_name` --desired-count 1 --health-check-grace-period-seconds 30"
+echo "running command aws ecs update-service --region ${TF_VAR_region} --cluster `terraform output cluster_name` --service `terraform output service_name` --desired-count 1 --health-check-grace-period-seconds 30"
 
-aws ecs update-service --cluster `terraform output cluster_name` --service `terraform output service_name` --desired-count 1 --health-check-grace-period-seconds 30
+aws ecs update-service --region ${TF_VAR_region} --cluster `terraform output cluster_name` --service `terraform output service_name` --desired-count 1 --health-check-grace-period-seconds 30
